9670fc719cd9a89237fb9a28b6ef6763888be0df
Minhnguyen376 <24020236@vnu.edu.vn>
2025-04-18 01:33:50 +0700

    Update renderwindow.cpp

--- src/renderwindow.cpp
+++ src/renderwindow.cpp
@@ -6,23 +6,25 @@
 #include "renderwindow.h"
 #include "entity.h"
 
+//screen size
 const int SCREEN_WIDTH = 1000;
 const int SCREEN_HEIGHT = 480;
 
 RenderWindow::RenderWindow()
 {
 }
-
+//tạo cửa sổ và render
 void RenderWindow::create(const char *p_title, int p_w, int p_h)
 {
+	//dùng SDL
 	window = SDL_CreateWindow(p_title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, p_w, p_h, SDL_WINDOW_SHOWN);
 
 	if (window == NULL)
 		std::cout << "Window failed to init. Error: " << SDL_GetError() << std::endl;
-
+	// Tạo renderer
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 }
-
+// lấy ảnh
 SDL_Texture *RenderWindow::loadTexture(const char *p_filePath)
 {
 	SDL_Texture *texture = NULL;
@@ -34,13 +36,14 @@ SDL_Texture *RenderWindow::loadTexture(const char *p_filePath)
 	return texture;
 }
 
+// reset nền
 void RenderWindow::clear()
 {
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); //đen
 	SDL_RenderClear(renderer);
 }
 
-void RenderWindow::render(Entity &p_entity)
+void RenderWindow::render(Entity &p_entity) //vẽ entity 
 {
 
 	for (int i = p_entity.getSize() - 1; i >= 0; i--)
@@ -68,10 +71,11 @@ void RenderWindow::render(Entity &p_entity)
 			dst.y -= 32;
 		}
 
-		SDL_RenderCopy(renderer, p_entity.getTex(i), &src, &dst);
+		// Đẩy texture lên renderer
+		SDL_RenderCopy(renderer, p_entity.getTex(i), &src, &dst); 
 	}
 }
-
+// Vẽ texture đơn (không dùng Entity) tại toạ độ p_x, p_y
 void RenderWindow::render(float p_x, float p_y, SDL_Texture *p_tex)
 {
 	SDL_Rect src;
@@ -87,18 +91,18 @@ void RenderWindow::render(float p_x, float p_y, SDL_Texture *p_tex)
 
 	SDL_RenderCopy(renderer, p_tex, &src, &dst);
 }
-
+// Vẽ toàn màn hình texture
 void RenderWindow::render(SDL_Texture *p_tex)
 {
 	render(0, 0, p_tex);
 }
-
+// hiện chữ từ string tại vị trí (p_x, p_y)
 void RenderWindow::render(float p_x, float p_y, const std::string p_text, TTF_Font *font, SDL_Color textColor)
 {
 	char *cstr = new char[p_text.length() + 1];
 	strcpy(cstr, p_text.c_str());
-	SDL_Surface *surfaceMessage = TTF_RenderText_Blended(font, cstr, textColor);
-	SDL_Texture *message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+	SDL_Surface *surfaceMessage = TTF_RenderText_Blended(font, cstr, textColor); //surface từ text
+	SDL_Texture *message = SDL_CreateTextureFromSurface(renderer, surfaceMessage); // texture từ surface
 
 	SDL_Rect src;
 	src.x = 0;
@@ -112,11 +116,11 @@ void RenderWindow::render(float p_x, float p_y, const std::string p_text, TTF_Fo
 	dst.w = src.w;
 	dst.h = src.h;
 
-	SDL_RenderCopy(renderer, message, &src, &dst);
-	SDL_FreeSurface(surfaceMessage);
-	SDL_DestroyTexture(message);
+	SDL_RenderCopy(renderer, message, &src, &dst); //Hiện chữ lên màn
+	SDL_FreeSurface(surfaceMessage); // Dọn dẹp bộ nhớ
+	SDL_DestroyTexture(message); 
 }
-
+// Hiện chữ từ const char* tại vị trí (p_x, p_y)
 void RenderWindow::render(float p_x, float p_y, const char *p_text, TTF_Font *font, SDL_Color textColor)
 {
 	SDL_Surface *surfaceMessage = TTF_RenderText_Blended(font, p_text, textColor);
@@ -138,7 +142,7 @@ void RenderWindow::render(float p_x, float p_y, const char *p_text, TTF_Font *fo
 	SDL_FreeSurface(surfaceMessage);
 	SDL_DestroyTexture(message);
 }
-
+// Hiện chữ ở chính giữa màn hình, thêm offset
 void RenderWindow::renderCenter(float p_x, float p_y, const std::string p_text, TTF_Font *font, SDL_Color textColor)
 {
 	char *cstr = new char[p_text.length() + 1];
@@ -162,7 +166,7 @@ void RenderWindow::renderCenter(float p_x, float p_y, const std::string p_text, 
 	SDL_FreeSurface(surfaceMessage);
 	SDL_DestroyTexture(message);
 }
-
+// Hiện chữ ở chính giữa từ const char*
 void RenderWindow::renderCenter(float p_x, float p_y, const char *p_text, TTF_Font *font, SDL_Color textColor)
 {
 	SDL_Surface *surfaceMessage = TTF_RenderText_Blended(font, p_text, textColor);
@@ -184,17 +188,17 @@ void RenderWindow::renderCenter(float p_x, float p_y, const char *p_text, TTF_Fo
 	SDL_FreeSurface(surfaceMessage);
 	SDL_DestroyTexture(message);
 }
-
+// Display đã render ra màn hình
 void RenderWindow::display()
 {
 	SDL_RenderPresent(renderer);
 }
-
+// clear res
 void RenderWindow::cleanUp()
 {
 	SDL_DestroyWindow(window);
 }
-
+// Đổi bg color cho renderer
 void RenderWindow::Color(int r, int g, int b, int opacity)
 {
 	SDL_SetRenderDrawColor(renderer, r, g, b, opacity);
